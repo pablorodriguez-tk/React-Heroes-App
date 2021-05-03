f3dae78e38e848796ab33e237a40efdd3151434b
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-03 02:50:33 -0300

    Add list of heroes

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const LoginScreen = () => {
+const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    history.replace("/");
+  };
+
   return (
-    <div>
+    <div className="container mt-5">
       <h1>LoginScreen</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 };
---
+++ src/selectors/getHeroesById.js
@@ -0,0 +1,7 @@
+import { heroes } from "../data/heroes";
+
+const getHeroesById = ({ id }) => {
+  return heroes.find((hero) => hero.id === id);
+};
+
+export default getHeroesById;
