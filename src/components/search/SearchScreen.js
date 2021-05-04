20971266059bb8b24ce6c6ec2f3344c641dd7eb8
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-04 02:01:10 -0300

    Solve problem in searchScreen and navbar

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useMemo } from "react";
+import queryString from "query-string";
 import { useLocation } from "react-router";
-import { heroes } from "../../data/heroes";
 import useForm from "../../hooks/useForm";
 import HeroCard from "../heroes/HeroCard";
+import getHeroesByName from "../../selectors/getHeroesByName";
 
 const SearchScreen = ({ history }) => {
   const location = useLocation();
+  const { q = "" } = queryString.parse(location.search);
 
-  const heroesFiltered = heroes;
-
-  const [formValues, handleInputChange] = useForm({ searchText: "" });
+  const [formValues, handleInputChange] = useForm({ searchText: q });
 
   const { searchText } = formValues;
+
+  const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -47,6 +49,13 @@ const SearchScreen = ({ history }) => {
         </div>
         <div className="col-7">
           <h4>Results</h4>
+          <hr />
+          {q === "" && <div className="alert alert-info">Search a hero</div>}
+          {q !== "" && heroesFiltered.length === 0 && (
+            <div className="alert alert-danger">
+              There is no a hero with {q}
+            </div>
+          )}
           {heroesFiltered.map((hero) => (
             <HeroCard key={hero.id} {...hero} />
           ))}
